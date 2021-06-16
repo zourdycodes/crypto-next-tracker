9d18c6e33addc3fb0d8000eda36045d4f9e42c3d
muhammadzourdy <zhafarmuhammad12@gmail.com>
2021-06-16 21:49:03 +0700

    styling the coin crypto list

--- components/Coins.js
+++ components/Coins.js
@@ -1,3 +1,5 @@
+import styles from "./styles/Coins.module.css";
+
 const Coins = ({
   name,
   price,
@@ -8,23 +10,29 @@ const Coins = ({
   priceChange,
 }) => {
   return (
-    <div className="coin__container">
-      <div className="coin__row">
-        <div className="coin">
-          <img src={image} alt={name} className="coin__img" />
-          <h1 className="coin__title">{name}</h1>
-          <p className="coin__symbol">{symbol}</p>
+    <div className={styles.coin__container}>
+      <div className={styles.coin__row}>
+        <div className={styles.coin}>
+          <img src={image} alt={name} className={styles.coin__img} />
+          <h1 className={styles.coin__title}>{name}</h1>
+          <p className={styles.coin__symbol}>{symbol}</p>
         </div>
 
-        <div className="coin__data">
-          <p className="coin__price">${+price.toFixed(1)}</p>
-          <p className="coin__volume">${volume.toLocaleString()}</p>
+        <div className={styles.coin__data}>
+          <p className={styles.coin__price}>${+price.toFixed(1)}</p>
+          <p className={styles.coin__volume}>${volume.toLocaleString()}</p>
 
           {priceChange < 0 ? (
-            <p className="coin__change red">{priceChange.toFixed(2)}%</p>
+            <p className={`${styles.coin__change} ${styles.red}`}>
+              {priceChange.toFixed(2)}%
+            </p>
           ) : (
-            <p className="coin__change green">{priceChange.toFixed(2)}%</p>
+            <p className={`${styles.coin__change} ${styles.green}`}>
+              {priceChange.toFixed(2)}%
+            </p>
           )}
+
+          <p className="market__cap">Mkt cap: ${marketcap.toLocaleString()}</p>
         </div>
       </div>
     </div>
